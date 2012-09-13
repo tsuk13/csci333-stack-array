372e405be2db3085f6c4d1de448cb81be57e9f0f
Travace Reese <tsuk13@gmail.com>
2012-09-12 21:28:37 -0400

    set up a auto-resizing stack test

--- test/Stack.cpp
+++ test/Stack.cpp
@@ -6,7 +6,13 @@ TEST(StackTest, Push) {
   s->push(6);
 
   EXPECT_EQ(6, s->peek());
+
+  for(int i = 0; i<50; ++i){
+    s->push(i);
+  }
+  for(int i =0; i<50; ++i){
+    EXPECT_EQ(49-i, s->pop());
+  } 
+  EXPECT_EQ(6, s->peek());
   delete s;
 }
-
-
